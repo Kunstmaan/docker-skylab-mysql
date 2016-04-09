b55ac4ff414063e8ea3f06db7d4b89d6a8ace1d4
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2016-04-09 16:09:22 +0200

    Fix pipe

--- 5.6/backup.sh
+++ 5.6/backup.sh
@@ -4,7 +4,7 @@ echo "SET autocommit=0;" > /docker-entrypoint-initdb.d/mysql.sql
 echo "SET unique_checks=0;" >> /docker-entrypoint-initdb.d/mysql.sql
 echo "SET foreign_key_checks=0;" >> /docker-entrypoint-initdb.d/mysql.sql
 mysqldump --skip-opt --add-drop-table --add-locks --create-options --disable-keys --single-transaction --skip-extended-insert --quick --set-charset -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE >> /docker-entrypoint-initdb.d/mysql.sql
-echo "COMMIT;" > /docker-entrypoint-initdb.d/mysql.sql
+echo "COMMIT;" >> /docker-entrypoint-initdb.d/mysql.sql
 echo "SET autocommit=1;" >> /docker-entrypoint-initdb.d/mysql.sql
 echo "SET unique_checks=1;" >> /docker-entrypoint-initdb.d/mysql.sql
 echo "SET foreign_key_checks=1;" >> /docker-entrypoint-initdb.d/mysql.sql
